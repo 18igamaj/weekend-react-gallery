b75ef4b4a3c7e3d3055f9e610375bdeb5312a886
Majid Igale <majidigale18@gmail.com>
2023-06-02 18:19:34 -0500

    created images database

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
+import {useState,useEffect} from 'react'
+
 
 function App() {
+
+const [listImg, setListImg] = useState([])
+
+useEffect(() => {
+  getImages()
+}, [])
+
+const getImages = () => {
+  axios.get('/gallery')
+  .then(response => {
+    console.log(response.data)
+    setListImg(response.data)
+    console.log(listImg)
+  }).catch(err => {
+    console.log('Something is wrong with our get', err)
+  })
+}
+
     return (
-      <div className="App">
+      <div >
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
+
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <div >
+          {listImg.map( image => (
+            <div key={image.id}>
+              <img src={image.path} />
+              {/* <p>{image.title}</p> */}
+              <br></br>
+              <button>Like</button>
+            </div>
+          ))}
+        </div>
+      
       </div>
     );
 }
